4528e3074bb3d7cab1d67b3cf925d104e0a81d94
Pawel Maliszewski <pablo999455@gmail.com>
2023-01-09 08:16:03 +0100

    solution 1.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,17 +3,8 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Monitor monitor1 = new Monitor();
-        monitor1.producer = "HP";
-        monitor1.modelNumber = "ADI-5646";
-        monitor1.screenSize = 20;
-        monitor1.price = 699.99;
-
-        Monitor monitor2 = new Monitor();
-        monitor2.producer = "DELL";
-        monitor2.modelNumber = "YRE-87965";
-        monitor2.screenSize = 23;
-        monitor2.price = 854.99;
+        Monitor monitor1 = new Monitor("HP", "ADI-5646", 20, 699.99);
+        Monitor monitor2 = new Monitor("DELL", "YRE-87965", 23, 854.99);
 
         monitor1.showInfo();
         monitor2.showInfo();
--- src/main/java/pl/javastart/task/Monitor.java
+++ src/main/java/pl/javastart/task/Monitor.java
@@ -6,6 +6,13 @@ public class Monitor {
     int screenSize;
     double price;
 
+    public Monitor(String producer, String modelNumber, int screenSize, double price) {
+        this.producer = producer;
+        this.modelNumber = modelNumber;
+        this.screenSize = screenSize;
+        this.price = price;
+    }
+
     void showInfo() {
         System.out.println("Monitor marki: " + producer + "\n"
                 + "Model: " + modelNumber + "\n"
